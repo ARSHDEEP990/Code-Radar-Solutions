2790b48386103bc8e6cee30bc0581f23f04a501b
ARSHDEEP990 <arshdeepsingh63920@gmail.com>
2025-02-08 20:42:58 +0530

    Solution to Check if Two Numbers Have Same Sign

--- Check if Two Numbers Have Same Sign@@6788dd63490637cfc44e1ba6/code.c
+++ Check if Two Numbers Have Same Sign@@6788dd63490637cfc44e1ba6/code.c
@@ -8,6 +8,9 @@ int main(){
     if(n>=0 && b>=0){
         printf("Same Sign");
     }
+    else{
+        printf("Different Sign");
+    }
     if(n<=0 && b<=0){
         printf("Same Sign");
     }
