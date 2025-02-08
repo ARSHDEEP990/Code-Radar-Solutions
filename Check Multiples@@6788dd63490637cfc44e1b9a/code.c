57db8f9da4898af1128cdbbba66624bf1095fc53
ARSHDEEP990 <arshdeepsingh63920@gmail.com>
2025-02-08 21:10:39 +0530

    Solution to Check Multiples

--- Check Multiples@@6788dd63490637cfc44e1b9a/code.c
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -4,9 +4,9 @@ int main() {
     int num1, num2;
 
     // Input two numbers
-    printf("Enter the first number: ");
+
     scanf("%d", &num1);
-    printf("Enter the second number: ");
+
     scanf("%d", &num2);
 
     // Check if num1 is a multiple of num2
