dd77ada6c0caa6c9619766d75b774d14012ff35c
ARSHDEEP990 <arshdeepsingh63920@gmail.com>
2025-03-11 18:20:20 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int fibonacciSeries(int n){
+    if(n==0) return 0;
+    if(n==1 && n==2) return 1;
+    int recAns = fibonacciSeries(n-1)+fibonacciSeries(n-2);
+    return recAns;
+}
