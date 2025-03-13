be1d272474367c381a9510879f9f4fbb33a00a1f
ARSHDEEP990 <arshdeepsingh63920@gmail.com>
2025-03-13 14:10:51 +0530

    Solution to Insertion Sort for Integers

---
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Function to sort and print an array in ascending order
+void insertionSort(int arr[], int size) {
+    // Sorting using Bubble Sort
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // Print sorted array
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
